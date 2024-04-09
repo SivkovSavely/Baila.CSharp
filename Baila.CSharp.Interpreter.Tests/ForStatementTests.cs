b10f4d4406edead3bd4c39cef337537d08c94add
SivkovSavely <sivkov.00@gmail.com>
2024-04-09 20:15:46 +0500

    Add Baila.CSharp.Interpreter.Tests.ForStatementTests

---
+++ Baila.CSharp.Interpreter.Tests/ForStatementTests.cs
@@ -0,0 +1,26 @@
+using Baila.CSharp.Interpreter.StatementInterpreters;
+using Baila.CSharp.Interpreter.Stdlib;
+
+namespace Baila.CSharp.Interpreter.Tests;
+
+public class ForStatementTests
+{
+    public ForStatementTests()
+    {
+        NameTable.CurrentScope = new NameTable.Scope(); // clear scope
+    }
+    
+    [Fact]
+    public void X()
+    {
+        var lexer = new Lexer.Lexer("""
+                                    for i = 1 to 3 {}
+                                    """, "");
+        var parser = new Parser.Parser(lexer.Tokenize());
+        var stmt = parser.BuildAst();
+
+        Assert.False(NameTable.Exists("i"), "Loop counter should not exist at this point in time");
+        stmt.InterpretExecute();
+        Assert.False(NameTable.Exists("i"), "Loop counter should not be exposed to the scope after loop ran");
+    }
+}

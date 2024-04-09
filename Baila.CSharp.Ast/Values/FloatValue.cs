c3723b9644037c7325612877c8d170438e469f4f
SivkovSavely <sivkov.00@gmail.com>
2024-04-09 19:43:04 +0500

    Add FloatValue

---
+++ Baila.CSharp.Ast/Values/FloatValue.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using Baila.CSharp.Typing;
+
+namespace Baila.CSharp.Ast.Values;
+
+public class FloatValue(double value) : IValue
+{
+    public long GetAsInteger()
+    {
+        return (long)value;
+    }
+
+    public double GetAsFloat()
+    {
+        return value;
+    }
+
+    public bool GetAsBoolean()
+    {
+        return value != 0;
+    }
+
+    public string GetAsString()
+    {
+        return value.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public BailaType GetBailaType()
+    {
+        return BailaType.Float;
+    }
+}

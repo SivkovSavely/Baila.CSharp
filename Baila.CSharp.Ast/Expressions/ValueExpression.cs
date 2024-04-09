b534060bd7b5f6209d2b1161799cf01ab83b2b8d
SivkovSavely <sivkov.00@gmail.com>
2024-04-09 20:09:08 +0500

    Make ValueExpression.Value public

--- Baila.CSharp.Ast/Expressions/ValueExpression.cs
+++ Baila.CSharp.Ast/Expressions/ValueExpression.cs
@@ -4,13 +4,15 @@ namespace Baila.CSharp.Ast.Expressions;
 
 public class ValueExpression(IValue value) : IExpression
 {
+    public IValue Value { get; } = value;
+
     public string Stringify()
     {
-        return value.GetAsString();
+        return Value.GetAsString();
     }
 
     public override string ToString()
     {
-        return $"ValueExpression({value})";
+        return $"ValueExpression({Value})";
     }
 }

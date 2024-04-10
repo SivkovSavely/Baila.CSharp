e900c1c5e68473d5684a8d007474cb59cee80da3
SivkovSavely <sivkov.00@gmail.com>
2024-04-10 15:39:59 +0500

    Add ConstantDefineStatement and implement its interpreter

---
+++ Baila.CSharp.Ast/Statements/ConstantDefineStatement.cs
@@ -0,0 +1,15 @@
+﻿using Baila.CSharp.Ast.Expressions;
+
+namespace Baila.CSharp.Ast.Statements;
+
+public class ConstantDefineStatement(string name, IExpression value) : IStatement
+{
+    public string Name { get; } = name;
+    public IExpression Value { get; } = value;
+
+    public override string ToString()
+    {
+        return $"ConstantDefineStatement(Name={Name}, " +
+               $"Value={Value})";
+    }
+}
---
+++ Baila.CSharp.Interpreter/StatementInterpreters/ConstantDefineStatementInterpreter.cs
@@ -0,0 +1,13 @@
+﻿using Baila.CSharp.Ast.Statements;
+using Baila.CSharp.Interpreter.ExpressionInterpreters;
+using Baila.CSharp.Interpreter.Stdlib;
+
+namespace Baila.CSharp.Interpreter.StatementInterpreters;
+
+public class ConstantDefineStatementInterpreter : StatementInterpreterBase<ConstantDefineStatement>
+{
+    public override void Interpret(ConstantDefineStatement statement)
+    {
+        NameTable.AddConstant(statement.Name, statement.Value.InterpretEvaluate());
+    }
+}

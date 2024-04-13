faecc7ab53ec25780b8523cf25ab4fea87ac205c
SivkovSavely <sivkov.00@gmail.com>
2024-04-13 22:36:00 +0500

    Allow void return type in FunctionDefineStatement

--- Baila.CSharp.Ast/Statements/FunctionDefineStatement.cs
+++ Baila.CSharp.Ast/Statements/FunctionDefineStatement.cs
@@ -7,12 +7,12 @@ public class FunctionDefineStatement(
     string name,
     List<FunctionDefineStatement.FunctionParameter> parameters,
     IStatement body,
-    BailaType returnType) : IStatement
+    BailaType? returnType) : IStatement
 {
     public string Name { get; } = name;
     public List<FunctionParameter> Parameters { get; } = parameters;
     public IStatement Body { get; } = body;
-    public BailaType ReturnType { get; } = returnType;
+    public BailaType? ReturnType { get; } = returnType;
     
     public record FunctionParameter(string Name, BailaType Type, IExpression? DefaultValue, bool Vararg);
 

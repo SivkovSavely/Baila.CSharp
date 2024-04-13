39ac493ef45e3e4e3efb8d9aef059e16c5cc7907
SivkovSavely <sivkov.00@gmail.com>
2024-04-13 22:25:04 +0500

    Further improve Token.ToString()

--- Baila.CSharp.Lexer/Token.cs
+++ Baila.CSharp.Lexer/Token.cs
@@ -4,6 +4,6 @@ public record Token(Cursor Cursor, TokenType Type, string? Value = null)
 {
     public override string ToString()
     {
-        return Type == TokenType.Identifier ? Value! : Type.ToString();
+        return Type == TokenType.Identifier ? Value! : Type.Type;
     }
 }

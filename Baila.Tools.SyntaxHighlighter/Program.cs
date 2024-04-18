ec952c3d6068f1d141b5908df783f16d34b17c45
SivkovSavely <sivkov.00@gmail.com>
2024-04-18 16:40:21 +0500

    Add Baila.Tools.SyntaxHighlighter project

---
+++ Baila.Tools.SyntaxHighlighter/Program.cs
@@ -0,0 +1,27 @@
+﻿using Baila.CSharp.Lexer;
+
+if (args.Length >= 1)
+{
+    var path = Path.GetFullPath(args[0]);
+
+    if (!Directory.Exists(path))
+    {
+        Console.WriteLine($"Folder {path} doesn't exist");
+        return 1;
+    }
+
+    var files = Directory.GetFiles(path);
+    foreach (var file in files)
+    {
+        HighlightFile(file);
+    }
+}
+
+return 0;
+
+void HighlightFile(string filePath)
+{
+    var source = File.ReadAllText(filePath);
+    var lexer = new Lexer(source, filePath);
+    var tokens = lexer.Tokenize();
+}

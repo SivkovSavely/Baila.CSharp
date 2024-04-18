3c99a16e5f0716c5f89257c5d5e73ae4f2b2c9ce
SivkovSavely <sivkov.00@gmail.com>
2024-04-18 18:06:11 +0500

    Start implementing a syntax highlighter

--- Baila.Tools.SyntaxHighlighter/Program.cs
+++ Baila.Tools.SyntaxHighlighter/Program.cs
@@ -1,4 +1,5 @@
-﻿using Baila.CSharp.Lexer;
+﻿using System.Text;
+using Baila.CSharp.Lexer;
 
 if (args.Length >= 1)
 {
@@ -10,7 +11,7 @@ if (args.Length >= 1)
         return 1;
     }
 
-    var files = Directory.GetFiles(path);
+    var files = Directory.GetFiles(path, "*.baila");
     foreach (var file in files)
     {
         HighlightFile(file);
@@ -22,6 +23,77 @@ return 0;
 void HighlightFile(string filePath)
 {
     var source = File.ReadAllText(filePath);
-    var lexer = new Lexer(source, filePath);
+    var lexer = new Lexer(source, filePath, LexerMode.Highlighting);
     var tokens = lexer.Tokenize();
+
+    var html = new StringBuilder();
+    html.Append("<pre>");
+
+    foreach (var token in tokens)
+    {
+        if (token.Type == TokenType.Whitespace)
+        {
+            switch (token.Value)
+            {
+                case " ":
+                    AppendSpan("whitespace", token.Value!);
+                    break;
+                case "\n":
+                    html.Append("<br/>");
+                    break;
+            }
+        }
+        else if (token.Type == TokenType.EndOfLine)
+        {
+            html.Append("<br/>");
+        }
+        else if (token.Type == TokenType.SingleLineComment)
+        {
+            AppendSpan("comment", "#" + token.Value);
+        }
+        else if (token.Type == TokenType.MultiLineComment)
+        {
+            AppendSpan("comment", "/*" + token.Value + "*/");
+        }
+        else if (token.Type == TokenType.Identifier)
+        {
+            AppendSpan("identifier", token.Value!);
+        }
+        else
+        {
+            AppendSpan("unknown", token.Value ?? token.Type.Type);
+        }
+    }
+
+    html.AppendLine("</pre>");
+
+    html.AppendLine("""
+                    <style>
+                    pre {
+                      margin: 1em;
+                      padding: 1em;
+                      border-radius: 8px;
+                      background-color: rgba(0, 0, 0, 0.025);
+                      box-shadow: 0 5px 10px 5px rgba(0, 0, 0, 0.2);
+                    }
+                    
+                    .unknown { color: red; font-style: italic; }
+                    .comment { color: gray; font-style: italic; }
+                    </style>
+                    """);
+
+    var htmlFilePath = Path.Combine(
+        Path.GetDirectoryName(filePath)!,
+        Path.GetFileNameWithoutExtension(filePath) + ".html");
+
+    File.WriteAllText(htmlFilePath, html.ToString());
+
+    return;
+
+    void AppendSpan(string className, string contents)
+    {
+        html.Append($"""
+                     <span class="{className}">{contents}</span>
+                     """);
+    }
 }

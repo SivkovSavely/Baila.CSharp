76f3be004ea5594b4ae6c1427fc417425b27004a
SivkovSavely <sivkov.00@gmail.com>
2024-04-18 16:40:33 +0500

    Add LexerMode.Highlighting and doc comments

--- Baila.CSharp.Lexer/LexerMode.cs
+++ Baila.CSharp.Lexer/LexerMode.cs
@@ -2,6 +2,16 @@
 
 public enum LexerMode
 {
+    /// <summary>
+    /// Regular lexer mode for interpreting/compiling.
+    /// </summary>
     Regular,
-    InterpolatedString
+    /// <summary>
+    /// Lexer mode for the inside of an interpolated string literal template.
+    /// </summary>
+    InterpolatedString,
+    /// <summary>
+    /// Lexer mode for the syntax highlighter. Includes whitespace and comments.
+    /// </summary>
+    Highlighting,
 }

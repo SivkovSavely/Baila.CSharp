fbfbf87ae314da8de97a68da8cc806eac7be4075
SivkovSavely <sivkov.00@gmail.com>
2024-04-25 21:00:31 +0500

    Implement better for statement tests

--- Baila.CSharp.Tests/ForStatementTests.cs
+++ Baila.CSharp.Tests/ForStatementTests.cs
@@ -1,5 +1,13 @@
+using Baila.CSharp.Ast.Expressions;
+using Baila.CSharp.Ast.Functional;
+using Baila.CSharp.Ast.Statements;
 using Baila.CSharp.Interpreter.Stdlib;
+using Baila.CSharp.Runtime.Values;
 using Baila.CSharp.Tests.Infrastructure;
+using Baila.CSharp.Typing;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
 using Xunit.Abstractions;
 
 namespace Baila.CSharp.Tests;
@@ -11,7 +19,46 @@ public class ForStatementTests : TestsBase
     }
 
     [Fact]
-    public void X()
+    public void TestParsing()
+    {
+        var program = CompileProgram("""
+                                     for i = 1 to 3 {
+                                        body();
+                                     }
+                                     """);
+
+        AssertAst(
+            program,
+            a =>
+            {
+                a.AssertRootStmt<ForStatement>(
+                    stmt =>
+                    {
+                        stmt.CounterVariable.Should().Be("i");
+                        a.AssertInner<IntValueExpression>(
+                            stmt.InitialValue,
+                            ive => ive.Value == 1);
+                        a.AssertInner<IntValueExpression>(
+                            stmt.FinalValue,
+                            ive => ive.Value == 3);
+                        a.AssertInnerBlock(
+                            stmt.Body,
+                            (_, innerAsserter) =>
+                            {
+                                innerAsserter.AssertRootExprStmt<FunctionCallExpression>(
+                                    fce =>
+                                    {
+                                        innerAsserter.AssertInner<VariableExpression>(
+                                            fce.FunctionHolder,
+                                            ve => ve.Name == "body");
+                                    });
+                            });
+                    });
+            });
+    }
+
+    [Fact]
+    public void TestCounterScopes()
     {
         var program = CompileProgram("""
                                      for i = 1 to 3 {}
@@ -20,5 +67,34 @@ public class ForStatementTests : TestsBase
         Assert.False(NameTable.Exists("i"), "Loop counter should not exist at this point in time");
         program.Execute();
         Assert.False(NameTable.Exists("i"), "Loop counter should not be exposed to the scope after loop ran");
+    }
+
+    [Fact]
+    public void TestUpperBoundInclusivity()
+    {
+        var program = CompileProgram("""
+                                     for i = 1 to 5 { print(i) }
+                                     """);
+
+        var counts = 0;
+
+        var callback = Substitute.For<IBailaCallable>();
+        callback.Call(Arg.Any<BailaCallableArgs>()).ReturnsNull().AndDoes(_ => { counts++; });
+
+        var function = new FunctionValue();
+        function.AddOverload(
+            new FunctionOverload(
+                callback,
+                [
+                    new FunctionParameter("x", BailaType.Any)
+                ],
+                null
+            )
+        );
+        NameTable.AddConstant("print", function);
+
+        program.Execute();
+
+        counts.Should().Be(5);
     }
 }

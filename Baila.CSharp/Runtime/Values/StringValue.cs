fa161a981ac22bac4e4befb5533f255c2c7e8e74
SivkovSavely <sivkov.00@gmail.com>
2024-04-26 03:28:12 +0500

    Add StringValue.FromRepeated method

--- Baila.CSharp/Runtime/Values/StringValue.cs
+++ Baila.CSharp/Runtime/Values/StringValue.cs
@@ -1,10 +1,28 @@
-﻿using Baila.CSharp.Runtime.Values.Abstractions;
+﻿using System.Text;
+using Baila.CSharp.Runtime.Values.Abstractions;
 using Baila.CSharp.Typing;
 
 namespace Baila.CSharp.Runtime.Values;
 
 public class StringValue(string value) : IValue
 {
+    public static StringValue FromRepeated(string value, long repetitionCount)
+    {
+        if (repetitionCount < 0)
+        {
+            throw new Exception("Repetition count cannot be less than zero");
+        }
+
+        var sb = new StringBuilder();
+
+        for (var i = 0L; i < repetitionCount; i++)
+        {
+            sb.Append(value);
+        }
+
+        return new StringValue(sb.ToString());
+    }
+    
     public long GetAsInteger()
     {
         return long.Parse(value);

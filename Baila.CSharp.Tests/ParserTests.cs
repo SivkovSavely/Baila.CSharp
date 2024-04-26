50677789f9c7e94646aedec0e0909c87fd7e5fe1
SivkovSavely <sivkov.00@gmail.com>
2024-04-26 15:27:38 +0500

    Remove if tests from ParserTests

--- Baila.CSharp.Tests/ParserTests.cs
+++ Baila.CSharp.Tests/ParserTests.cs
@@ -9,33 +9,4 @@ public class ParserTests : TestsBase
     public ParserTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
     {
     }
-
-    [Fact]
-    public void IfElseStatementTest_TrueBranchOnly_EmptyTrueBranch()
-    {
-        var statements = CompileProgram("""
-                                        if expr {}
-                                        """);
-        var statement = statements.StatementList.First();
-
-        var ifElseStatement = Assert.IsType<IfElseStatement>(statement);
-        var trueStatement = Assert.IsType<BlockStatement>(ifElseStatement.TrueStatement);
-        Assert.Empty(trueStatement.Statements);
-        Assert.Null(ifElseStatement.FalseStatement);
-    }
-
-    [Fact]
-    public void IfElseStatementTest_TrueBranchAndFalseBranch_EmptyBranches()
-    {
-        var statements = CompileProgram("""
-                                        if expr {} else {}
-                                        """);
-        var statement = statements.StatementList.First();
-
-        var ifElseStatement = Assert.IsType<IfElseStatement>(statement);
-        var trueStatement = Assert.IsType<BlockStatement>(ifElseStatement.TrueStatement);
-        Assert.Empty(trueStatement.Statements);
-        var falseStatement = Assert.IsType<BlockStatement>(ifElseStatement.FalseStatement);
-        Assert.Empty(falseStatement.Statements);
-    }
 }
